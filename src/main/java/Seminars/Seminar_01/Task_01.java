6c3c8709abee6b15813b4f39b47b4fbf6b9b15d9
K_aLex-a <shiotoy@icloud.com>
2023-07-26 20:23:06 +0300

    Seminar_01/Task_01 reworked

--- src/main/java/Seminars/Seminar_01/Task_01.java
+++ src/main/java/Seminars/Seminar_01/Task_01.java
@@ -14,18 +14,35 @@ package Seminars.Seminar_01;
 * "Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
 * "Доброй ночи, <Имя>!", если время от 23:00 до 4:59
 =======================================================*/
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Task_01 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String hello = "Hello, ";
-        String name = in.nextLine();
-
         System.out.print("Input your name: ");
+        String name = in.nextLine();
+        in.close();
+
         System.out.println();
-        System.out.println(hello + name);
+        System.out.println("Hello, " + name + "!");
+        time(name);
 
 
+    }
+
+    public static void time(String name) {
+        LocalTime time = LocalTime.now();
+        if (time.isAfter(LocalTime.of(5, 0)) && time.isBefore(LocalTime.of(11, 59))) {     // если текущее время больше 5:00 утра
+            System.out.println("Good morning, " + name + "!");
+        } else if (time.isAfter(LocalTime.of(12, 0)) && time.isBefore(LocalTime.of(17, 59))) {
+            System.out.println("Good afternoon, " + name + "!");
+        } else if (time.isAfter(LocalTime.of(18, 0)) && time.isBefore(LocalTime.of(22, 59))) {
+            System.out.println("Good evening, " + name + "!");
+        }else if (time.isAfter(LocalTime.of(23, 0)) && time.isBefore(LocalTime.of(4, 59))) {
+            System.out.println("Good night, " + name + "!");
+        }
     }
 }
