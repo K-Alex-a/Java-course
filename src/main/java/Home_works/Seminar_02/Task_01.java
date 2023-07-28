bcb976d1d20987e2546370d68696ee9ead3d6918
K_aLex-a <shiotoy@icloud.com>
2023-07-28 03:04:13 +0300

    task_01(Seminar_02) complete

--- src/main/java/Home_works/Seminar_02/Task_01.java
+++ src/main/java/Home_works/Seminar_02/Task_01.java
@@ -1,4 +1,5 @@
 package Home_works.Seminar_02;
+import java.util.Arrays;
 /*=======================================================================
 * Дана строка sql-запроса:
 * select * from students where "
@@ -14,13 +15,35 @@ package Home_works.Seminar_02;
 =======================================================================*/
 public class Task_01 {
     public static void main(String[] args) {
-        String QUERY = "select * from students where ";
-        String PARAMS = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"} ";
-        System.out.println(PARAMS);
+        String QUERY = "";
+        String PARAMS = "";
+
+        if (args.length == 0) {
+            QUERY = "select * from students where ";
+            PARAMS = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"null\", \"age\":\"18\"} ";
+        } else {
+            QUERY = args[0];
+            PARAMS = args[1];
+        }
+        System.out.println(Answer.answer(QUERY, PARAMS));
 
     }
+}
 
+class Answer {
     public static StringBuilder answer(String QUERY, String PARAMS) {
-        return null;
+        StringBuilder sb = new StringBuilder(QUERY);
+        String[] params = PARAMS.replaceAll("\"", "").split("[ {,}]");
+        System.out.println(Arrays.toString(params));
+        String[] dictionary;
+
+        for (String item : params) {
+            dictionary = item.split(":");
+            if (dictionary.length == 2 && !dictionary[1].contains("null")) {
+                sb.append(dictionary[0]).append("='").append(dictionary[1]).append("' and ");
+            }
+        }
+        sb.delete(sb.length() - 5, sb.length());
+        return sb;
     }
 }
