038742a8ab6a7c102b45a8736215ef1f49b33dbf
K_aLex-a <shiotoy@icloud.com>
2023-11-16 00:06:35 +0300

    complete task_03(Home_works/Additional_tasks)

--- src/main/java/Home_works/Additional_tasks/Task_02.java
+++ src/main/java/Home_works/Additional_tasks/Task_02.java
@@ -1,8 +1,12 @@
 package Home_works.Additional_tasks;
+
+import java.util.Arrays;
+import java.util.Random;
+
 /*===============================================================================
 * Объявлена доска 9х9. Необходимо отвалидировать в соответствии с правилами:
-* 1. каждая строка должна содержать цифру 1-9 ьез повторения
-* 2. каждая колонка должна содержать цифру 1-9 ьез повторения
+* 1. каждая строка должна содержать цифру 1-9 без повторения
+* 2. каждая колонка должна содержать цифру 1-9 без повторения
 * 3. каждый под блок из 9 элемнтов 3х3 должна содержать цифру 1-9 без повторения.
 * Ограничения: board.length == 9
 *              board[i].length == 9
@@ -17,4 +21,39 @@ package Home_works.Additional_tasks;
 * .....
 ===============================================================================*/
 public class Task_02 {
+    public static void main(String[] args) {
+        Integer[][] board = new Integer[9][9];
+        printArray(fillArray(board));
+        System.out.println();
+//        printArray(validationArray(board));
+
+    }
+
+
+    public static Integer[][] fillArray(Integer[][] board) {
+        Random rand = new Random();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = rand.nextInt(10);
+            }
+        }
+        return board;
+    }
+
+    public static void printArray(Integer[][] board) {
+        for (Integer[] integers : board) {
+            System.out.println(Arrays.toString(integers));
+        }
+    }
+
+//    public static Integer[][] validationArray(Integer[][] board) {
+//
+//        for (int i = 1; i < board.length; i++) {
+//            for (int j = 1; j < board.length; j++) {
+//
+//                }
+//            }
+//        return board;
+//        }
+
 }
--- src/main/java/Home_works/Additional_tasks/Task_03.java
+++ src/main/java/Home_works/Additional_tasks/Task_03.java
@@ -1,5 +1,7 @@
 package Home_works.Additional_tasks;
 
+
+import java.util.*;
 /*===========================================
 * Вывести список на экран в перевернутом виде
 * (без массивов и ArrayList).
@@ -7,4 +9,36 @@ package Home_works.Additional_tasks;
 * Вывод:  4 -> 3 -> 2 -> 1
 ===========================================*/
 public class Task_03 {
+    public static void main(String[] args) {
+        HashSet<Integer> nums = new HashSet<>();
+        nums.add(5);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+
+        System.out.println(nums);
+        printList((LinkedHashSet<Integer>) reversSet(nums));
+    }
+
+    public static Set<Integer> reversSet(HashSet<Integer> nums) {
+        TreeSet<Integer> sortedNums = new TreeSet<>(nums);
+        int length = sortedNums.size();
+        Set<Integer> newSetNums = new LinkedHashSet<>(length);
+        for (int i = 0; i < length; i++) {
+            newSetNums.add(sortedNums.pollLast());
+        }
+        return newSetNums;
+    }
+
+    public static void printList(LinkedHashSet<Integer> nums) {
+        Iterator<Integer> it = nums.iterator();
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == nums.size()-1) {
+                System.out.print(it.next());
+            } else {
+                System.out.print(it.next() + " -> ");
+            }
+        }
+    }
+
 }
